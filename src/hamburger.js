2f0bf36776e8caaa1232ff618ffe1c8ed37b0c5b
trubkokrtek <agentdominik707@gmail.com>
2025-04-10 18:10:30 +0200

    close rework

--- src/hamburger.js
+++ src/hamburger.js
@@ -2,21 +2,17 @@ document.addEventListener("click", (e) => {
     if (e.target.closest(".chatbot--icon")) {
         const chatbot = document.querySelector(".chatbot");
         const chatbotswitch = document.querySelector(".chatbot--icon");
-        const chatbotclose = document.querySelector(".chatbot--close");
         if (chatbot) {
             chatbot.classList.toggle("active");
             chatbotswitch.classList.toggle("active");
-            chatbotclose.classList.toggle("active");
         }
     }
     if (e.target.closest(".chatbot--close")) {
         const chatbot = document.querySelector(".chatbot");
         const chatbotswitch = document.querySelector(".chatbot--icon");
-        const chatbotclose = document.querySelector(".chatbot--close");
         if (chatbot) {
             chatbot.classList.toggle("active");
             chatbotswitch.classList.toggle("active");
-            chatbotclose.classList.toggle("active");
         }
     }
 });
