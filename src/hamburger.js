cbac1636ce28ee57c4b479e72c0c0fdea47c4b18
trubkokrtek <agentdominik707@gmail.com>
2025-04-10 21:23:33 +0200

    added text field

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,3 +1,6 @@
+
+
+
 document.addEventListener("click", (e) => {
     if (e.target.closest(".chatbot--icon")) {
         const chatbot = document.querySelector(".chatbot");
@@ -6,6 +9,11 @@ document.addEventListener("click", (e) => {
             chatbot.classList.toggle("active");
             chatbotswitch.classList.toggle("active");
         }
+        var inputfield = document.querySelector(".user-input");
+        if (inputfield.value == null) {}
+        else{
+            var inputvalue = inputfield.value;
+        }
     }
     if (e.target.closest(".chatbot--close")) {
         const chatbot = document.querySelector(".chatbot");
@@ -13,6 +21,25 @@ document.addEventListener("click", (e) => {
         if (chatbot) {
             chatbot.classList.toggle("active");
             chatbotswitch.classList.toggle("active");
+        }
+    }
+    if (e.target.closest(".user__input--btn")) {
+        try {
+            var textfield = document.querySelector(".user__input--field");
+            var inputvalue = textfield.value;
+            console.log(inputvalue);
+            textfield.value = "";
+            alert(inputvalue);
+            
+            const node = document.querySelector(".user").firstChild;
+            const clone = node.cloneNode(true);
+            clone.value = inputvalue;
+
+            const list = document.querySelector(".user");
+            list.insertBefore(clone, list.firstChild);
+        }
+        catch (error) {
+            console.error(error);
         }
     }
 });
