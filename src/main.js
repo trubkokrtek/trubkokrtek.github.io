660d1edc7801c48b54aa2f124c9b6ef559c907f0
trubkokrtek <agentdominik707@gmail.com>
2025-04-11 09:49:07 +0200

    logo rework

--- src/main.js
+++ src/main.js
@@ -18,6 +18,63 @@ const links = {
     "lyže": "https://www.skijested.cz/"
 }
 
+function chatbot() {
+    try {
+        var textfield = document.querySelector(".user__input--field");
+        var inputvalue = textfield.value;
+        console.log(inputvalue);
+        textfield.value = "";
+        //alert(inputvalue);
+        if (inputvalue == "") {return;}
+        const para = document.createElement("p");
+        para.textContent = inputvalue;
+        para.classList.add("user__text");
+        para.value = inputvalue;
+        const clone = para.cloneNode(true);
+        const list = document.querySelector(".user");
+        list.appendChild(para);
+        
+        const defaultText = document.querySelectorAll(".def");
+        for (var text of defaultText) {
+            text.remove();
+        }
+
+        const bot = document.querySelector(".bot");
+        var inputlist = inputvalue.split(" ");
+        for(var string of inputlist){
+            if (string.toLowerCase() == "ahoj" || string.toLowerCase() == "zdar" || string.toLowerCase() == "čau") {
+                const para = document.createElement("p");
+                para.textContent = "Ahoj, jak ti mohu pomoci?";
+                para.classList.add("bot__text");
+                bot.appendChild(para);
+            }
+            else if( string.toLowerCase() == "kuk") {
+                const para = document.createElement("p");
+                para.textContent = "Kuk";
+                para.classList.add("bot__text");
+                bot.appendChild(para);
+            }
+            else if (links[string.toLowerCase()]) {
+                const link = document.createElement("a");
+                const span = document.createElement("span");
+                span.classList.add("bot--link");
+                span.textContent = "zde";
+                link.href = links[string.toLowerCase()];
+                link.id = string.toLowerCase();
+                link.target = "_blank";
+                link.textContent = "Pokud hledáš informace o " + string + ", klikni ";
+                link.appendChild(span);
+                link.classList.add("bot__text");
+                
+                bot.appendChild(link);
+            }
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
 
 document.addEventListener("click", (e) => {
     if (e.target.closest(".chatbot--icon")) {
@@ -43,54 +100,7 @@ document.addEventListener("click", (e) => {
         }
     }
     if (e.target.closest(".user__input--btn")) {
-        try {
-            var textfield = document.querySelector(".user__input--field");
-            var inputvalue = textfield.value;
-            console.log(inputvalue);
-            textfield.value = "";
-            //alert(inputvalue);
-            if (inputvalue == "") {return;}
-            const para = document.createElement("p");
-            para.textContent = inputvalue;
-            para.classList.add("user__text");
-            para.value = inputvalue;
-            const clone = para.cloneNode(true);
-            const list = document.querySelector(".user");
-            list.appendChild(para);
-            
-            const defaultText = document.querySelectorAll(".def");
-            for (var text of defaultText) {
-                text.remove();
-            }
-
-            const bot = document.querySelector(".bot");
-            var inputlist = inputvalue.split(" ");
-            for(var string of inputlist){
-                if (string.toLowerCase() == "ahoj" || string.toLowerCase() == "zdar" || string.toLowerCase() == "čau") {
-                    const para = document.createElement("p");
-                    para.textContent = "Ahoj, jak ti mohu pomoci?";
-                    para.classList.add("bot__text");
-                    bot.appendChild(para);
-                }
-                else if (links[string.toLowerCase()]) {
-                    const link = document.createElement("a");
-                    const span = document.createElement("span");
-                    span.classList.add("bot--link");
-                    span.textContent = "zde";
-                    link.href = links[string.toLowerCase()];
-                    link.id = string.toLowerCase();
-                    link.target = "_blank";
-                    link.textContent = "Pokud hledáš informace o " + string + ", klikni ";
-                    link.appendChild(span);
-                    link.classList.add("bot__text");
-                    
-                    bot.appendChild(link);
-                }
-            }
-        }
-        catch (error) {
-            console.error(error);
-        }
+        chatbot();   
     }
     if (e.target.closest(".bot--link")) {
         if (e.target.closest.id == null) {
@@ -101,3 +111,8 @@ document.addEventListener("click", (e) => {
         }
     }
 });
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        chatbot();
+    }
+});	
